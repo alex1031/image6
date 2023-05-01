1db78bda408a19c61a5539ecb695b5ce07322630
jmur3881 <jessicarose1.murrie@gmail.com>
2023-05-01 10:47:26 +1000

    Update to R

--- Image_App/app.R
+++ Image_App/app.R
@@ -1,4 +1,3 @@
-#
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
@@ -8,7 +7,13 @@
 #
 
 library(shiny)
+library(shinydashboard)
 library(rdrop2)
+#library(keras)
+
+# Load the model
+#model <- load_model_tf("cnn-mnist/")
+
 
 ui <- fluidPage(
   
@@ -17,7 +22,21 @@ ui <- fluidPage(
   navbarPage(
     "Effects of Robustness",
     
-    tabPanel("Introduction"),
+    tags$head(
+      tags$style(HTML("
+      /* Set the width and height of the app */
+      .shiny-output {
+        width: 1000px;
+        height: 800px;
+      }
+    "))
+    ),
+    
+    
+    tabPanel("Introduction",
+             align = "center",
+             mainPanel(
+             )),
     
     tabPanel("Noise Levels",
              sidebarLayout(
@@ -25,13 +44,12 @@ ui <- fluidPage(
                  sliderInput("noise-level", "Noise Levels:", min = 0, max = 1, value = 0.2)
                ),
                mainPanel(
-                 tabsetPanel(
-                   tabPanel("Performance"),
-                   tabPanel("Mutated Image")
+                 # Output: Histogram ----
+                 #textOutput(outputId = "prediction"),
+                 #plotOutput(outputId = "image")
                  )
                )
-             )
-    ), 
+             ),
     
     tabPanel("Image Resolution",
              sidebarLayout(
@@ -39,13 +57,12 @@ ui <- fluidPage(
                  sliderInput("resolution", "Resolution:", min = 1, max = 150, value = 55)
                ),
                mainPanel(
-                 tabsetPanel(
-                   tabPanel("Performance"),
-                   tabPanel("Mutated Image")
+                 # Output: Histogram ----
+                 #textOutput(outputId = "prediction"),
+                 #plotOutput(outputId = "image")
                  )
                )
-             )
-    ), 
+             ),
     
     tabPanel("Image Rotation",
              sidebarLayout(
@@ -53,32 +70,60 @@ ui <- fluidPage(
                  sliderInput("rotation", "Rotation:", min = 0, max = 360, value = 60)
                ),
                mainPanel(
-                 tabsetPanel(
-                   tabPanel("Performance"),
-                   tabPanel("Mutated Image")
+                 # Output: Histogram ----
+                 #textOutput(outputId = "prediction"),
+                 #plotOutput(outputId = "image")
                  )
                )
-             )
-    ),
+             ),
     
     tabPanel("Demonstration",
              fluidRow(
                column(4,
                       fileInput("file", h3("File input")), 
-                      sliderInput("noise-level-demo", "Noise Level", min = 0, max = 1, value = 0.2),
-                      sliderInput("resolution-demo", "Resolution", min = 1, max = 150, value = 55),
-                      sliderInput("rotation-demo", "Rotation", min = 0, max = 360, value = 60)
-                      )
+               ),
+               mainPanel(
+                 # Output: Histogram ----
+                 #textOutput(outputId = "prediction"),
+                 #plotOutput(outputId = "image")),
+                 
+               actionButton("change", "change")
+               
              )
     )
   )
 )
+)
 
 server <- function(input, output) {
+  
+  randomVals <- eventReactive(input$change, {
+    runif(input$file)
+    
+  })
+    
+    image <- reactive({
+      req(input$file)
+      jpeg::readJPEG(input$file$datapath)
+      
+    })
+    
+    #output$prediction <- renderText({
+      
+      #img <- image() %>% 
+        #array_reshape(., dim = c(1, dim(.), 1))
+      
+      #paste0("The predicted class number is ", predict_classes(model, img))
+    ##})
+    
+    output$image <- renderPlot({
+      plot(as.raster(image()))
+    })
+    
+  
   
 }
 
 shinyApp(ui = ui, server = server)
-
 
 
